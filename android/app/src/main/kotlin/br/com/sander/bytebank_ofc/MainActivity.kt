5d77f72e8988fdf13fab61a349a4c8318fa0b954
Phellipe Sander <phellipe.sander1@gmail.com>
2022-03-10 19:33:53 -0300

    creating project

---
+++ android/app/src/main/kotlin/br/com/sander/bytebank_ofc/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.sander.bytebank_ofc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+  const ByteBankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Dashboard'),
+        ),
+      ),
+    );
+  }
+}
